c33c0fe5a0ede090f27c5456dc00ae00d3696a74
Romy-code <71374125+Romy-code@users.noreply.github.com>
2021-05-05 09:38:41 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace InvasionSpatiale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.envahisseur = new System.Windows.Forms.PictureBox();
+            this.joueur = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.envahisseur)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.joueur)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // envahisseur
+            // 
+            this.envahisseur.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.envahisseur.Location = new System.Drawing.Point(622, 12);
+            this.envahisseur.Name = "envahisseur";
+            this.envahisseur.Size = new System.Drawing.Size(50, 50);
+            this.envahisseur.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.envahisseur.TabIndex = 1;
+            this.envahisseur.TabStop = false;
+            this.envahisseur.Tag = "envahisseur";
+            // 
+            // joueur
+            // 
+            this.joueur.Image = global::InvasionSpatiale.Properties.Resources.cercle;
+            this.joueur.Location = new System.Drawing.Point(271, 411);
+            this.joueur.Name = "joueur";
+            this.joueur.Size = new System.Drawing.Size(50, 50);
+            this.joueur.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.joueur.TabIndex = 0;
+            this.joueur.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox1.Location = new System.Drawing.Point(61, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "envahisseur";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox2.Location = new System.Drawing.Point(117, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "envahisseur";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox3.Location = new System.Drawing.Point(173, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "envahisseur";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox4.Location = new System.Drawing.Point(229, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "envahisseur";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox5.Location = new System.Drawing.Point(285, 12);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "envahisseur";
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox6.Location = new System.Drawing.Point(341, 12);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "envahisseur";
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox7.Location = new System.Drawing.Point(398, 12);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 8;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "envahisseur";
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox8.Location = new System.Drawing.Point(454, 12);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 9;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "envahisseur";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox9.Location = new System.Drawing.Point(510, 12);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 10;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Tag = "envahisseur";
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox10.Location = new System.Drawing.Point(566, 12);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 11;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Tag = "envahisseur";
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = global::InvasionSpatiale.Properties.Resources.posters_visage_de_dessin_anime_drole_de_monstre_monstre_creature_avatar_stock_de_vecteur_jpg;
+            this.pictureBox11.Location = new System.Drawing.Point(5, 12);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 12;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Tag = "envahisseur";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 439);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Score: 00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.envahisseur);
+            this.Controls.Add(this.joueur);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "InvasionSpatiale";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.envahisseur)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.joueur)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox joueur;
+        private System.Windows.Forms.PictureBox envahisseur;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvasionSpatiale
+{
+    public partial class Form1 : Form
+    {
+        bool gauche;
+
+        bool droite;
+
+        int vitesse = 5;
+
+        int score = 0;
+
+        bool estAppuye;
+
+        int totalEnemies = 12;
+
+        int vitesseJoueur = 6;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                gauche = true;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                droite = true;
+            }
+            if (e.KeyCode == Keys.Space && !estAppuye)
+            {
+                estAppuye = true;
+
+                creationBalles();
+            }
+        }
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+
+                gauche = false;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+
+                droite = false;
+            }
+            if (estAppuye)
+            {
+                estAppuye = false;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (gauche)
+            {
+                joueur.Left -= vitesseJoueur;
+            }
+            else if (droite)
+            {
+                joueur.Left += vitesseJoueur;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+
+                if (x is PictureBox && x.Tag == "envahisseur")
+                {
+                    if (((PictureBox)x).Bounds.IntersectsWith(joueur.Bounds))
+                    {
+                        perdu();
+
+                    }
+
+                          ((PictureBox)x).Left += vitesse;
+
+                    if (((PictureBox)x).Left > 720)
+                    {
+                        ((PictureBox)x).Top += ((PictureBox)x).Height + 10;
+
+                        ((PictureBox)x).Left = -50;
+                    }
+                }
+            }
+        }
+
+        private void creationBalles()
+        {
+            PictureBox balle = new PictureBox();
+
+            balle.Image = Properties.Resources.balle;
+
+            balle.Size = new Size(5, 20);
+
+            balle.Tag = "bullet";
+
+            balle.Left = joueur.Left + joueur.Width / 2;
+
+            balle.Top = joueur.Top - 20;
+
+            this.Controls.Add(balle);
+
+            balle.BringToFront();
+
+        }
+
+        private void perdu()
+        {
+            timer1.Stop();
+            label1.Text += " Vous avez perdu";
+        }
+    }
+}
